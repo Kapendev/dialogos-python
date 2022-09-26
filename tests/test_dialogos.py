e27a42926981be1b3121257eba91243a42451bfa
AlexandrosKap <alexandroskapretsos@gmail.com>
2022-09-27 00:12:57 +0300

    better tests

--- tests/test_dialogos.py
+++ tests/test_dialogos.py
@@ -1,15 +1,48 @@
+from itertools import combinations, permutations
 from dialogos import *
 
-# TODO: Add more tests.
 
-def test_calc():
-    assert calc("") is None
-    assert calc("ada") is None
-    assert calc("oh+wow") is None
-    assert calc("oh*wow") is None
-    assert calc("()") is None
-    assert calc("()+()") is None
-    assert calc("()*()") is None
-    assert calc("1") == 1.0
-    assert calc("(1)") == 1.0
-    assert calc("((1))") == 1.0
+# Test the functions for processing strings.
+
+
+def test_calc_for_none() -> None:
+    values = ("", "(", ")", "()", "(())", "+", "*", "vim", "nim", "-1", "+1")
+    for l, r in permutations(values, 2):
+        for op in values:
+            s = f"{l}{op}{r}"
+            assert calc(s) is None
+
+
+def test_calc_for_some() -> None:
+    values = (
+        "0",
+        "11",
+        "22 * 10",
+        "(35)",
+        "((17))",
+        "(1 + 1)",
+        "((17 - 29))",
+        "(53 + 23 / (78 + 14 * (94 * 2) + 3))",
+    )
+    ops = ("+", "*")
+    for l, r in combinations(values, 2):
+        for op in ops:
+            s = f"{l}{op}{r}"
+            n1 = calc(l)
+            n2 = calc(r)
+            n3 = calc(s)
+            if n1 is not None and n2 is not None and n3 is not None:
+                if op == "+":
+                    assert n3 == n1 + n2
+                elif op == "*":
+                    assert n3 == n1 * n2
+            else:
+                assert False
+
+
+def test_split() -> None:
+    assert split("") == []
+    assert split("      orb|| bro  ") == ["orb", " bro"]
+
+
+# TODO: Test the dialogue struct.
